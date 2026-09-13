80d28239dd1c22f0c023494e15191682fc8e65c9
jugnumittal4-netizen <jugnumittal4@gmail.com>
2026-09-13 13:10:18 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-cd = {"india":91,"usa":1,"canada":1,"pakistan":92,"australia":61}
-print("the country code for india is:",cd["india"])
-print("the country code for usa is:",cd["usa"])
-print("the country code for canada is:",cd["canada"])
-print("the country code for pakistan is:",cd["pakistan"])
-print("the country code for australia is:",cd["australia"])
+st = {"id1": {"name": "Alice", "class": "10th", "subject": "math,english,hindi"},
+      "id2": {"name": "Bob", "class": "10th", "subject": "science,history,geography"},
+      "id3": {"name": "charlie", "class": "10th", "subject": "math,english,hindi"},
+      "id4": {"name": "ashu", "class": "10th", "subject": "math,punjabi,hindi"}}
+print("Original Dictionary:")
+print(st)
+print("")
+print("id:1",st.get("id1","not found"))
+print("")
+print("detalis about id:5")
+print(st.get("id5","not found"))
+st["id2"]["subject"] = "math,english,hindi"
+print("")
+print("after changing subject of id:2")
+print(st.get("id2", "not found"))
+st["id5"] = {"name": "david", "class": "10th", "subject": "math,english,hindi"}
+print("")
+cd = {}
+sr = []
+for st , i in st.items():
+    unique_key = (i['name'], i['class'], i['subject'])
+    if unique_key not in sr:
+        sr.append(unique_key)
+        cd[st] = i
+sd = cd
+print("after removing duplicates:")
+print(sd)
+rs = sd.pop("id3", "not found")
+print("")
+print("after removing id:3")
+print(sd)
+print("")
+print("total students left",len(sd))
+print("")
+print("========final subject report========")
+for si, details in sd.items():
+    print(f"{si}: {details}")
+
